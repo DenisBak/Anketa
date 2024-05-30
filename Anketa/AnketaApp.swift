ede9af673cbc173ce3ff03636304293216ba392c
Artyom <ivashchenkodevelopment@gmail.com>
2024-05-30 21:44:38 +0500

    Initial Commit

---
+++ Anketa/AnketaApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnketaApp.swift
+//  Anketa
+//
+//  Created by Артём Иващенко on 30/05/24.
+//
+
+import SwiftUI
+
+@main
+struct AnketaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
